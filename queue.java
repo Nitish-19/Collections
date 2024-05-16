05849391f382d4800c1adfc06444aee78cb3c661
Nitish kumar <113499545+Nitish-19@users.noreply.github.com>
2024-05-16 17:06:03 +0530

    Add files via upload

---
+++ queue.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.LinkedList;
+import java.util.Queue;//FIFO
+
+public class queue {
+
+	public static void main(String[] args) {
+		Queue <String> st = new LinkedList <> ();
+		st.offer("loin");
+		st.offer("cat");
+		st.offer("dog");
+		st.offer("donkey");
+		st.offer("elephant");
+		
+		System.out.println(st);
+		
+		System.out.println(st.peek());
+		
+		st.poll();
+		System.out.println(st.peek());
+
+	
+	
+	}
+
+}
---
+++ stack.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.Stack;//push,pope,peak  
+
+public class stack {
+
+	public static void main(String[] args) {
+		Stack <String> st = new Stack <> ();
+		st.push("loin");
+		st.push("cat");
+		st.push("dog");
+		st.push("donkey");
+		st.push("elephant");
+		
+		System.out.println(st);
+		
+		System.out.println(st.peek());
+		
+		st.pop();
+		System.out.println(st.peek());
+
+	
+
+	}
+
+}
